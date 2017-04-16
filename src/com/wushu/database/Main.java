c0ed6a6c6de7b726782a621f538e0839bdd76493
brahim <ibra.bahri@gmail.com>
2017-04-16 17:03:11 +0200

    first commit

---
+++ src/com/wushu/database/Main.java
@@ -0,0 +1,8 @@
+package com.wushu.database;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+    	DBConnector dao = new DBConnector();
+        dao.readDataBase();
+}
+}
